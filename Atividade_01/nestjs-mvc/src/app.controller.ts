7a946b7ce40a4453e3403ea6a905b7410b98d66c
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2023-08-15 22:15:49 -0300

    Enfeitando um pouco mais...

--- Atividade_01/nestjs-mvc/src/app.controller.ts
+++ Atividade_01/nestjs-mvc/src/app.controller.ts
@@ -1,10 +1,25 @@
 import { Get, Controller, Render } from '@nestjs/common';
 
+
 @Controller()
 export class AppController {
   @Get()
   @Render('index')
   root() {
-    return { message: 'Hello world!' };
+    return {
+      messages: [
+        {
+          message: 'Hello World!!',
+          id: 'hello-world',
+          title: 'Hello World'
+        },
+        {
+          message: 'Olá Mundo!!',
+          id: 'ola-mundo',
+          title: 'Olá Mundo'
+        }
+      ]
+    };
   }
+
 }
