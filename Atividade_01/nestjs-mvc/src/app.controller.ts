35242d5c3d8113c1856918c150c1e52eaf761c47
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2023-08-29 21:16:25 -0300

    Arrumando algumas coisas..

--- Atividade_01/nestjs-mvc/src/app.controller.ts
+++ Atividade_01/nestjs-mvc/src/app.controller.ts
@@ -1,35 +1,162 @@
-import { Get, Controller, Render, Module } from '@nestjs/common';
-import { get } from 'http';
-import { AppService } from './app.service';
+import { Get, Controller, Render, Module, Injectable, Query, Post, Body, Res, Req, Redirect } from '@nestjs/common';
+import { AppService, Page } from './app.service';
 
-@Module({
-  controllers: [AppController],
-  providers: [AppService],
-})
 @Controller()
 export class AppController {
-  @Get()
+
+  constructor(private readonly appService: AppService) { }
+
+  @Get('/')
   @Render('index')
   home() {
-    // return this.AppService.getHome();
+    return {
+      anchors: [
+        {
+          name: 'Hello World',
+          href: '/hello-world'
+        },
+        {
+          name: 'Lab 1',
+          href: '/lab1'
+        },
+        {
+          name: 'Lab 2',
+          href: '/lab2'
+        }
+      ]
+    }
   }
-  // @Get()
-  // @Render('index')
-  // root() {
-  //   return {
-  //     messages: [
-  //       {
-  //         message: 'Hello World!!',
-  //         id: 'hello-world',
-  //         title: 'Hello World'
-  //       },
-  //       {
-  //         message: 'Olá Mundo!!',
-  //         id: 'ola-mundo',
-  //         title: 'Olá Mundo'
-  //       }
-  //     ]
-  //   };
-  // }
+
+  @Get('/lab1')
+  @Render('lab1')
+  homeLab1() {
+    const retorno: {
+      title: string,
+      anchor: Page[]
+    } = this.appService.getHome();
+
+    return {
+      title: retorno.title,
+      anchors: retorno.anchor,
+      main: {
+        title: 'Home'
+      },
+      link_css: [
+        "home.css",
+        "basic.css"
+      ]
+    };
+  }
+  @Get('/lab1/produtos')
+  @Render('lab1')
+  produtos(): {
+    title: string,
+    anchors: Page[],
+    main: {
+      title: string,
+      produtos: {
+        id: string,
+        nome: string,
+        status: string
+      }[]
+    },
+    link_css: string[]
+  } {
+    const retorno = this.appService.getProdutos();
+    return {
+      title: retorno.title,
+      anchors: retorno.pages,
+      main: {
+        title: 'Lista de Produtos',
+        produtos: retorno.produtos
+      },
+      link_css: [
+        "produtos.css",
+        "basic.css"
+      ]
+    };
+  }
+  @Get('/lab1/produtos/adicionar')
+  @Render('lab1')
+  produtos_adicionar(): {
+    title: string,
+    anchors: Page[],
+    main: {
+      title: string,
+      form: {
+        action: string,
+        method: string,
+        inputs: {
+          name: string,
+          type: string,
+          placeholder: string
+        }[],
+        submitText: string
+      }
+    },
+    link_css: string[]
+  } {
+    const retorno = this.appService.getProdutosAdicionar();
+    return {
+      title: retorno.title,
+      anchors: retorno.pages,
+      main: {
+        title: 'Adicionar Produto',
+        form: retorno.form
+      },
+      link_css: [
+        "produtos.css",
+        "basic.css"
+      ]
+    };
+  }
+
+  @Post('/lab1/produtos/adicionar')
+  @Render('lab1')
+  @Redirect('/lab1/produtos')
+  produto_adicionar(
+    @Body('nome') nome: string
+  ): void {
+    this.appService.addProduto({ nome });
+  }
+
+  @Get('/lab1/produtos/:id/remover')
+  @Render('index')
+  @Redirect('/lab1/produtos')
+  produto_remover(
+    @Query('id') id: string
+  ): void {
+    this.appService.removeProduto(parseInt(id));
+  }
+
+  @Get('/lab1/produtos/:id/mudar-status')
+  @Render('lab1')
+  @Redirect('/lab1/produtos')
+  produto_mudar_status(
+    @Query('id') id: string
+  ): void {
+    this.appService.mudarStatusProduto(parseInt(id));
+  }
+  @Get('hello-world')
+  @Render('index')
+  root() {
+    return {
+      messages: [
+        {
+          message: 'Hello World!!',
+          id: 'hello-world',
+          title: 'Hello World'
+        },
+        {
+          message: 'Olá Mundo!!',
+          id: 'ola-mundo',
+          title: 'Olá Mundo'
+        }
+      ],
+      link_css: [
+        "css/styles.css",
+      ]
+    };
+  }
 
 }
--- Atividade_01/nestjs-mvc/src/app.service.ts
+++ Atividade_01/nestjs-mvc/src/app.service.ts
@@ -5,6 +5,11 @@ enum Status {
   INATIVO = 'inativo'
 }
 
+export interface Page {
+  name: string,
+  href: string
+}
+
 class Produto {
   constructor(
     public id: number,
@@ -14,36 +19,30 @@ class Produto {
 }
 
 const Produtos: Produto[] = [];
+const pages: Page[] = [
+  {
+    name: 'Home',
+    href: '/'
+  },
+  {
+    name: 'Lista de Produtos',
+    href: 'produtos'
+  },
+  {
+    name: 'Adicionar Produto',
+    href: 'produtos/adicionar'
+  }
+]
+
 @Injectable()
 export class AppService {
   getHome(): {
     title: string,
-    anchor: {
-      title: string,
-      href: string
-    }[]
+    anchor: Page[]
   } {
     return {
       title: 'Home',
-      anchor: [
-
-        {
-          title: 'Home',
-          href: '/'
-        },
-        {
-          title: 'Lista de Produtos',
-          href: '/produtos'
-        },
-        {
-          title: 'Adicionar Produto',
-          href: '/produtos/adicionar'
-        },
-        {
-          title: 'Remover Produto',
-          href: '/produtos/remover'
-        }
-      ]
+      anchor: pages
     }
   }
 
@@ -52,28 +51,117 @@ export class AppService {
     produtos: {
       id: string,
       nome: string,
-      status: Status,
-      href: string
-    }[]
+      status: Status
+    }[],
+    pages: Page[]
   } {
     const prod: {
       id: string,
       nome: string,
-      status: Status,
-      href: string
+      status: Status
     }[] = [];
     Produtos.forEach((produto) => {
       prod.push({
         id: produto.id.toString(),
         nome: produto.nome,
-        status: produto.status,
-        href: `/produtos/${produto.id}`
+        status: produto.status
       })
     })
 
     return {
       title: 'Lista de Produtos',
-      produtos: prod
+      produtos: prod,
+      pages: pages
     }
+  }
+
+  getProduto(id: number): {
+    title: string,
+    produto: {
+      id: string,
+      nome: string,
+      status: Status
+    },
+    pages: Page[]
+  } {
+    const produto = Produtos.find((produto) => produto.id === id);
+
+    return {
+      title: 'Produto',
+      produto: {
+        id: produto.id.toString(),
+        nome: produto.nome,
+        status: produto.status
+      },
+      pages: pages
+    }
+  }
+
+  getProdutosAdicionar(): {
+    title: string,
+    form: {
+      action: string,
+      method: string,
+      inputs: {
+        name: string,
+        type: string,
+        placeholder: string
+      }[],
+      submitText: string
+    },
+    pages: Page[]
+  } {
+    return {
+      title: 'Adicionar Produto',
+      form: {
+        action: '/produtos/adicionar',
+        method: 'POST',
+        inputs: [
+          {
+            name: 'nome',
+            type: 'text',
+            placeholder: 'Nome do Produto'
+          }
+        ],
+        submitText: 'Adicionar'
+      },
+      pages: pages
+    }
+  }
+
+
+  addProduto(produto: {
+    nome: string
+  }): {
+    status: number
+  } {
+    const id: number = Produtos.length + 1;
+    const newProduto: Produto = new Produto(id, produto.nome, Status.ATIVO);
+    Produtos.push(newProduto);
+
+    return { status: 201 }
+  }
+
+  removeProduto(id: number): {
+    status: number
+  } {
+    const index = Produtos.findIndex((produto) => produto.id === id);
+    Produtos.splice(index, 1);
+
+    return { status: 200 }
+  }
+
+  mudarStatusProduto(id: number): {
+    status: number
+  } {
+    const index = Produtos.findIndex((produto) => produto.id === id);
+    if (index === -1) return { status: 404 }
+    if (Produtos[index].status === Status.ATIVO) {
+      Produtos[index].status = Status.INATIVO;
+    } else {
+      Produtos[index].status = Status.ATIVO;
+    }
+
+    return { status: 200 }
   }
 }
