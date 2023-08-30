56bda7049393c276709854c80f306afd6a26811d
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2023-08-30 12:45:19 -0300

    Update main.ts

--- Atividade_01/nestjs-mvc/src/main.ts
+++ Atividade_01/nestjs-mvc/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import * as hbs from 'hbs';
 
@@ -9,6 +10,15 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação dos Laboratórios')
+    .setDescription('Aqui estão os endpoints dos laboratórios..')
+    .setVersion('1.0')
+    .addTag('Labs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
