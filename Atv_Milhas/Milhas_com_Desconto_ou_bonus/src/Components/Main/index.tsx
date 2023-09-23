9bb14faf35eb1feee8f70a62e22db57d470c0d81
Marcos1701 <catce.2022111tads0428@aluno.ifpi.edu.br>
2023-09-22 22:42:28 -0300

    :moyai::wine_glass: Sofrendo/Pelejando para entender o que a atividade está pedindo ;-;...

--- Atv_Milhas/Milhas_com_Desconto_ou_bonus/src/Components/Main/index.tsx
+++ Atv_Milhas/Milhas_com_Desconto_ou_bonus/src/Components/Main/index.tsx
@@ -6,12 +6,14 @@ export function Main() {
 
   const [qtd_Milhas, setMilhas] = useState<number>(0)
   const [preco_milheiro, setPreco_milheiro] = useState<number>(0)
-  const [Bonus, setBonus] = useState<number>(0)
+  const [Desconto, setDesconto] = useState<number>(0)
   const [valor_cada_milheiro, setvalor_cada_milheiro] = useState<number>(0)
 
   useEffect(() => {
-    setvalor_cada_milheiro((preco_milheiro * (1 - (Bonus / 100))) / 1000)
-  }, [qtd_Milhas, preco_milheiro, Bonus]) // isso é um array de dependencias, ou seja, quando algum desses valores mudar, o useEffect será executado
+    const valor_cada_milheiro_com_desconto: number = preco_milheiro - (preco_milheiro * (1 - (Desconto / 100)))
+    console.log(valor_cada_milheiro_com_desconto)
+    setvalor_cada_milheiro(valor_cada_milheiro_com_desconto)
+  }, [qtd_Milhas, preco_milheiro, Desconto]) // isso é um array de dependencias, ou seja, quando algum desses valores mudar, o useEffect será executado
 
 
   return (
@@ -41,28 +43,31 @@ export function Main() {
         }} />
       </section>
 
-      <section className='Bonus_content'>
-        <p>Bonus</p>
-        <div id="Bonus">
-          <Input id='Bonus_input' type='number' Initialvalue={Bonus} onChange={({ target }: React.ChangeEvent<HTMLInputElement>) => {
+      <section className='Desconto_content'>
+        <p>Desconto</p>
+        <div id="Desconto">
+          <Input id='Desconto_input' type='number' Initialvalue={Desconto} onChange={({ target }: React.ChangeEvent<HTMLInputElement>) => {
             const num: number = parseInt(target.value);
-            if ((!isNaN(num) && num >= 0)) {
-              setBonus(num);
+            if ((!isNaN(num) && num >= 0 && num <= 100)) {
+              setDesconto(num);
             }
           }} />
-          <label htmlFor="Bonus_input"> %</label>
+          <label htmlFor="Desconto_input"> %</label>
         </div>
-        <div id="Buttons_bonus">
-          <Button id="bonus_80" text="80%" onClick={() => { setBonus(Bonus + 80) }} />
-          <Button id="bonus_100" text="100%" onClick={() => { setBonus(Bonus + 100) }} />
-          <Button id="bonus_150" text="150%" onClick={() => { setBonus(Bonus + 150) }} />
-          <Button id="bonus_200" text="200%" onClick={() => { setBonus(Bonus + 200) }} />
-          <Button id="bonus_300" text="300%" onClick={() => { setBonus(Bonus + 300) }} />
-        </div>
+        {/* <div id="Buttons_Desconto">
+          <Button id="Desconto_80" text="80%" onClick={() => {
+            setDesconto(
+              Desconto + 80 <= 100 &&
+                Desconto + 80 >= 0 ? Desconto + 80 : 100)
+          }} />
+          <Button id="Desconto_100" text="100%" onClick={() => {
+            setDesconto(100)
+          }} />
+        </div> */}
       </section>
 
       {
-        valor_cada_milheiro > 0 ?
+        valor_cada_milheiro > 0 && qtd_Milhas > 0 ?
           <section className='Resultado'>
             {valor_cada_milheiro >= 70 ?
               <div className='retorno_visual'>
@@ -87,22 +92,25 @@ export function Main() {
             }
 
             <div className="retorno_preco_milheiro">
-              <p>Cada milheiro sairá por <strong>R$ {valor_cada_milheiro}</strong></p>
+              <p>Cada milheiro sairá por <strong>R$ {valor_cada_milheiro.toPrecision(2)}</strong></p>
             </div>
 
             <div className="explicacao_final">
               <p>
                 *Sua compra de {qtd_Milhas} milhas custaria R$ {
                   (qtd_Milhas / 1000) * preco_milheiro
-                } porém com seu desconto de {Bonus} % pagará apenas R$ {
-                  (qtd_Milhas / 1000) * preco_milheiro * (1 - (Bonus / 100))
-                }.
+                } porém com seu desconto de {Desconto} % pagará apenas R$ {
+                  (qtd_Milhas / 1000) * preco_milheiro * (1 - (Desconto / 100)) < 0 ?
+                    0 :
+                    (qtd_Milhas / 1000) * preco_milheiro * (1 - (Desconto / 100))
+                }.<br />
                 <strong>
                   Economizou R$ {
-                    (qtd_Milhas / 1000) * preco_milheiro - (qtd_Milhas / 1000) * preco_milheiro * (1 - (Bonus / 100))
+                    (qtd_Milhas / 1000) * preco_milheiro - (qtd_Milhas / 1000) * (preco_milheiro * (1 - (Desconto / 100))) < 0 ?
+                      0 :
+                      (qtd_Milhas / 1000) * preco_milheiro - (qtd_Milhas / 1000) * (preco_milheiro * (1 - (Desconto / 100)))
                   }, {
-                    (qtd_Milhas / 1000) * preco_milheiro - (qtd_Milhas / 1000) * preco_milheiro * (1 - (Bonus / 100))
-                    / (qtd_Milhas / 1000) * preco_milheiro * (1 - (Bonus / 100)) * 100
+                    Desconto === 0 ? "0" : Desconto === 100 ? "100" : Desconto
                   } % a menos com seu desconto.
                 </strong>*
               </p>
